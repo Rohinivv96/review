f42fbe6b795b4aa07731177b65d86fd626353597
Rohinivv96 <vaidyarohini0006@gmail.com>
2022-04-10 12:18:12 +0530

    this is my first commit

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# go to the website
+#Search for a respective product(iphone)
+#try to loacte a review part
+#scrap all the data in local system
+
 from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
 import requests
@@ -79,5 +84,5 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(debug=True)
+    app.run(host='127.0.0.1', port=8001, debug=True)
+	#app.run(debug=True)
